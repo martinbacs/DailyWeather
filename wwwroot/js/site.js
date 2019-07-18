35b678d8aab53edc5718bc7e09eb593c8fc4500c
Martin Bäckström <marbac@Martins-MacBook-Pro.local>
2019-07-18 15:06:27 +0200

    added interactive map to Rainfall view

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var mymap = L.map('mapid').setView([62.161, 15.311], 4);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibWFyYmFjIiwiYSI6ImNqeTdvc2dmejAxbnAzaHAxOHF5ZWVzMXgifQ.G42i7HcUkTVvFKd-8rG5ZQ'
+    }).addTo(mymap);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
